229b0eac9317eb65827051b47b7f2cf9954940e4
jurph <jurph@nidoking.lan>
2019-03-02 11:30:08 -0500

    Mapping source and disassembly to understand control flow and functions

--- narnia/narnia7.c
+++ narnia/narnia7.c
@@ -19,27 +19,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int goodfunction();
-int hackedfunction();
-
+int goodfunction();  
+int hackedfunction(); // initialized next to each other.
+  
 int vuln(const char *format){
-        char buffer[128];
-        int (*ptrf)();
+        char buffer[128];   																		// 0x0804862b
+        int (*ptrf)(); 																				
 
-        memset(buffer, 0, sizeof(buffer));
-        printf("goodfunction() = %p\n", goodfunction);
-        printf("hackedfunction() = %p\n\n", hackedfunction);
+        memset(buffer, 0, sizeof(buffer));													// 0x0804862f
+        printf("goodfunction() = %p\n", goodfunction);								// 0x08048641
+        printf("hackedfunction() = %p\n\n", hackedfunction);						// 0x08048653
 
-        ptrf = goodfunction;
-        printf("before : ptrf() = %p (%p)\n", ptrf, &ptrf);
+        ptrf = goodfunction;																	// 0x0804865b
+        printf("before : ptrf() = %p (%p)\n", ptrf, &ptrf);								// 0x08048678
 
-        printf("I guess you want to come to the hackedfunction...\n");
-        sleep(2);
-        ptrf = goodfunction;
+        printf("I guess you want to come to the hackedfunction...\n");		// 0x08048490 - why does this get "puts()" instead of printf as expected?
+        sleep(2);																					// 0x08048470 - TODO: see if sleep() responds badly to weird input.
+        ptrf = goodfunction;																	// 0x08048697
 
-        snprintf(buffer, sizeof buffer, format);
+        snprintf(buffer, sizeof buffer, format);											// 0x080486ad
 
-        return ptrf();
+        return ptrf();																				// 0x080486be
 }
 
 int main(int argc, char **argv){
@@ -47,21 +47,21 @@ int main(int argc, char **argv){
                 fprintf(stderr, "Usage: %s <buffer>\n", argv[0]);
                 exit(-1);
         }
-        exit(vuln(argv[1]));
-}
+        exit(vuln(argv[1]));  																		// 0x080486f1 - wait, what?! We're exiting the whole time vuln() is running? 
+}																										// 						TODO: consider pointer implications.
 
 int goodfunction(){
         printf("Welcome to the goodfunction, but i said the Hackedfunction..\n");
-        fflush(stdout);
+        fflush(stdout);																			// 0x08048715
 
         return 0;
 }
 
-int hackedfunction(){
-        printf("Way to go!!!!");
-	    fflush(stdout);
-        setreuid(geteuid(),geteuid());
-        system("/bin/sh");
+int hackedfunction(){																			// 0x08048724
+        printf("Way to go!!!!");																	// 0x0804872d
+	    fflush(stdout);																			// 0x0804873b
+        setreuid(geteuid(),geteuid());														// 0x08048743 
+        system("/bin/sh");																		// 0x0804875e
 
         return 0;
 }
